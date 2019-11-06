a04393836da4773ed0671e033736bacc3fbf8c7d
Akash <mrudul@mabventures.com>
2019-11-06 12:44:53 +0530

    first commit

---
+++ WebbiContactform/src/Models/ContactForm.php
@@ -0,0 +1,10 @@
+<?php
+namespace Webbi\WebbiContactform\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactForm extends Model
+{
+    protected $guarded = [];
+
+    protected $table = 'contact';
+}
